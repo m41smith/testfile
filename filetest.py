e665dcbced3ee76670b296b7f3261011e593059f
m41smith <m41smith@student.bridgew.edu>
2020-10-05 23:31:04 -0400

    added number formatting

--- filetest.py
+++ filetest.py
@@ -7,11 +7,19 @@ def main():
     random_line = random.choice(all_of_file)
     print(random_line)
 
+
 def reverser():
     rev_file = open("Reversable.txt", "r")
     all_lines = rev_file.readlines()
     to_insert = all_lines[0]
     print(f"welcome to {to_insert}, fantastic student")
 
+
+def string_formating_demo():
+    result = 100/3
+    formatted_number = "{:5.3f}".format(result)
+    print(f"the result is {formatted_number}")
+
 #reverser()
-main()
+#main()
+string_formating_demo()
