750556e66c6a89df3bdafa88a44f845a624f9ffc
m41smith <m41smith@student.bridgew.edu>
2020-10-05 23:22:19 -0400

    added y lines

--- GraphPaper.py
+++ GraphPaper.py
@@ -10,7 +10,8 @@ def draw_lines():
     # draw here
     for location in range(80, width, 80):
         arcade.draw_line(location, 0, location, height, arcade.color.BLACK, 2)
-        arcade.draw_line(0, location, width, location, arcade.color.BLACK, 2)
+    for y_location in range(80, height, 80):
+        arcade.draw_line(0, y_location, width, y_location, arcade.color.BLACK, 2)
 
     arcade.finish_render()
 
